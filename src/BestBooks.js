e2d043092be5ce9c3e1cf20051789d9b6e6b7bd1
Timothy Parker <tlparker71@gmail.com>
2022-07-25 19:46:03 -0700

    trello lab 11 book component

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,17 +10,24 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    this.setState({books : axios('/books')}) ;
 
+  }
   render() {
 
     /* TODO: render all the books in a Carousel */
+      ;
+  
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <p>{this.state.books.carousel({
+                interval: 2000
+          })}</p>
         ) : (
           <h3>No Books Found :(</h3>
         )}
